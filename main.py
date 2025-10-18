49600768ce260cfa97c18195b4dce679c98c48a8
jundole05 <jundole05@gmail.com>
2025-10-18 14:22:29 +0900

    main문 수정

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from pico2d import *
 import game_framework
 
+import play_mode as start_mode
+
 open_canvas(1600, 1000)
 startscreen = load_image ("./Resource/startscreen/startscreen.png")
 startscreen.draw_now(800, 500)
+game_framework.run(start_mode)
 delay(5)
