cb8a7322a9f8ed0a94363ecb81e0356f6fe2fa09
jundole05 <jundole05@gmail.com>
2025-10-18 11:05:57 +0900

    change_mode 구현

--- Resource/game_framework.py
+++ Resource/game_framework.py
@@ -1,2 +1,17 @@
 import time
 frame_time = 0.0
+
+running = None
+stack = None
+
+def change_mode(mode):
+    global stack
+    if (len(stack) > 0):
+        # execute the current mode's finish function
+        stack[-1].finish()
+        # remove the current mode
+        stack.pop()
+    stack.append(mode)
+    mode.init()
+
+
