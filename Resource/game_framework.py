581929f76a1130f9afc23ce0e59181acc71ca70d
jundole05 <jundole05@gmail.com>
2025-10-18 11:08:30 +0900

    quit , run  구현

--- Resource/game_framework.py
+++ Resource/game_framework.py
@@ -32,3 +32,31 @@ def pop_mode():
     # execute resume function of the previous mode
     if (len(stack) > 0):
         stack[-1].resume()
+
+def quit():
+    global running
+    running = False
+
+def run(start_mode):
+    global running, stack
+    running = True
+    stack = [start_mode]
+    start_mode.init()
+
+    global frame_time
+    frame_time = 0.0
+    current_time = time.time()
+    while running:
+        stack[-1].handle_events()
+        stack[-1].update()
+        stack[-1].draw()
+
+        frame_time = time.time() - current_time
+        current_time += frame_time
+        frame_rate = 1.0 / frame_time
+        # print(f'Frame Time: {frame_time}, Frame Rate: {frame_rate}')
+
+    # repeatedly delete the top of the stack
+    while (len(stack) > 0):
+        stack[-1].finish()
+        stack.pop()
