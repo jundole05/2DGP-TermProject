d86f0a42a4f05f830b64a498740085c659eefcd4
jundole05 <jundole05@gmail.com>
2025-10-18 11:06:49 +0900

    pop_mode 구현

--- Resource/game_framework.py
+++ Resource/game_framework.py
@@ -20,3 +20,15 @@ def push_mode(mode):
         stack[-1].pause()
     stack.append(mode)
     mode.init()
+
+def pop_mode():
+    global stack
+    if (len(stack) > 0):
+        # execute the current mode's finish function
+        stack[-1].finish()
+        # remove the current mode
+        stack.pop()
+
+    # execute resume function of the previous mode
+    if (len(stack) > 0):
+        stack[-1].resume()
