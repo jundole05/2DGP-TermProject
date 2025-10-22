328d534925ffc834bdd7fe7cefac4c99c37d7645
jundole05 <jundole05@gmail.com>
2025-10-22 17:23:30 +0900

    위아래 방향키 추가

--- character.py
+++ character.py
@@ -11,6 +11,18 @@ def space_down(e): # e is space down ?
 
 time_out = lambda e: e[0] == 'TIMEOUT'
 
+def up_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_UP
+
+def up_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_UP
+
+def down_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_DOWN
+
+def down_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_DOWN
+
 def right_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
 
