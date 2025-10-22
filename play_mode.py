e4279693a7870d7de55799a274f49d04e2127e64
jundole05 <jundole05@gmail.com>
2025-10-22 12:37:28 +0900

    시작화면을 띄우고 좌클릭을 하면 시작화면이 지워지고 game_world가 render 되도록 수정

--- play_mode.py
+++ play_mode.py
@@ -4,24 +4,36 @@ from pico2d import *
 import game_framework
 import game_world
 
+startscreen = None
+show_startscreen = True
+
 def handle_events():
+    global show_startscreen
     event_list = get_events()
     for event in event_list:
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
+        elif event.type == SDL_MOUSEBUTTONDOWN and show_startscreen:
+            show_startscreen = False
 
 def init():
+    global startscreen
+    startscreen = load_image("./Resource/startscreen/startscreen.png")
     pass
 
 def update():
-    game_world.update()
-    game_world.handle_collisions()
+    if not show_startscreen:
+        game_world.update()
+        game_world.handle_collisions()
 
 def draw():
     clear_canvas()
-    game_world.render()
+    if show_startscreen:
+        startscreen.draw(800, 450)
+    else:
+        game_world.render()
     update_canvas()
 
 def finish():
