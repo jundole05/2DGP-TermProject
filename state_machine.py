94b587221bf8abbdaaccd01f93e8cf81d497a2ac
jundole05 <jundole05@gmail.com>
2025-10-22 12:16:35 +0900

    state_machine 구현

---
+++ state_machine.py
@@ -0,0 +1,23 @@
+
+class StateMachine:
+    def __init__(self, start_state, state_transitions):
+        self.cur_state = start_state
+        self.state_transitions = state_transitions
+        self.cur_state.enter(('START', None))
+
+    def update(self):
+        self.cur_state.do()
+
+    def handle_state_event(self, event):
+        for check_event in self.state_transitions[self.cur_state].keys():
+            if check_event(event):
+                self.cur_state.exit(event)
+                self.next_state = self.state_transitions[self.cur_state][check_event]
+                self.next_state.enter(event)
+                self.cur_state = self.next_state
+                return
+
+
+    def draw(self):
+        self.cur_state.draw()
+
