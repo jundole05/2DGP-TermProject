132bf2619a1acbe7fbc4e0796327cd1131b5d84d
jundole05 <jundole05@gmail.com>
2025-10-22 11:01:45 +0900

    character 클래스 작성

---
+++ character.py
@@ -0,0 +1,35 @@
+from pico2d import *
+from sdl2 import *
+
+import game_world
+import game_framework
+
+def space_down(e): # e is space down ?
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
+time_out = lambda e: e[0] == 'TIMEOUT'
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
+class character:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def handle_event(self, event):
+        pass
+
+    def draw(self):
+        pass
