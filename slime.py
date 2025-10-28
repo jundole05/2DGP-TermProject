bc8e46c3bdab1c150d3550607739090fbf7d53b4
jundole05 <jundole05@gmail.com>
2025-10-28 14:16:19 +0900

    slime class 구현

--- slime.py
+++ slime.py
@@ -64,3 +64,68 @@ class Run:
                       FRAME_W, FRAME_H,
                       self.slime.x, self.slime.y,
                       self.slime.draw_w, self.slime.draw_h)
+
+class Slime:
+    """
+    slime_type: 0,1,2 (각각의 이미지 쌍을 사용)
+    이미지 경로는 아래 SLIME_IMAGES 리스트에서 수정하십시오.
+    """
+    SLIME_IMAGES = [
+        ('./Resource/slime/slime1_idle.png', './Resource/slime/slime1_run.png'),
+        ('./Resource/slime/slime2_idle.png', './Resource/slime/slime2_run.png'),
+        ('./Resource/slime/slime3_idle.png', './Resource/slime/slime3_run.png'),
+    ]
+
+    def __init__(self, slime_type=0, x=100, y=100, draw_w=100, draw_h=100, speed=RUN_SPEED_PPS := 200):
+        # 이미지 로드 (파일 경로는 필요에 맞게 수정)
+        idle_path, run_path = Slime.SLIME_IMAGES[slime_type]
+        self.idle_image = load_image(idle_path)
+        self.run_image = load_image(run_path)
+
+        self.x, self.y = x, y
+        self.draw_w, self.draw_h = draw_w, draw_h
+        self.speed = speed
+
+        self.frame = 0
+        self.face_dir = random.randint(0, 3)  # 0:up,1:right,2:left,3:down
+        self.dir_x = 0
+        self.dir_y = 0
+
+        self.IDLE = Idle(self)
+        self.RUN = Run(self)
+        self.state_machine = StateMachine(
+            self.IDLE,
+            {
+                self.IDLE: {run_event: self.RUN},
+                self.RUN: {idle_event: self.IDLE}
+            }
+        )
+
+        self.change_timer = CHANGE_INTERVAL
+
+    def update(self):
+        # 상태별 동작(이동/프레임 업데이트)은 state에 맡김
+        self.state_machine.update()
+
+        # 주기 타이머 감소
+        self.change_timer -= game_framework.frame_time
+        if self.change_timer <= 0:
+            self.change_timer = CHANGE_INTERVAL
+            # 방향 변경
+            self.face_dir = random.randint(0, 3)
+            # 랜덤으로 run 또는 idle 선택
+            if random.choice([True, False]):
+                self.state_machine.handle_state_event(('RUN', None))
+            else:
+                self.state_machine.handle_state_event(('IDLE', None))
+
+    def draw(self):
+        self.state_machine.draw()
+
+    def get_bb(self):
+        half_w = self.draw_w / 2
+        half_h = self.draw_h / 2
+        return (self.x - half_w, self.y - half_h, self.x + half_w, self.y + half_h)
+
+    def handle_collision(self, group, other):
+        pass
