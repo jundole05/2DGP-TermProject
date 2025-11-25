f507377b464cd9f5f9b0c41e0ee0c14dec8f8ec2
jundole05 <jundole05@gmail.com>
2025-11-25 21:24:49 +0900

    slime의 behavior tree 수정

--- slime.py
+++ slime.py
@@ -192,15 +192,21 @@ class Slime:
         self.build_behavior_tree()
 
     def build_behavior_tree(self):
-        c1 = Condition('캐릭터가 근처에 있는가?', self.is_character_nearby, 5)
-        a1 = Action('캐릭터 추적', self.move_to_character)
-        chase_character = Sequence('캐릭터 추적', c1, a1)
+        c1 = Condition('캐릭터가 10 이내에 있는가?', self.is_character_nearby, 10)
+        c2 = Condition('캐릭터가 3 이내에 있는가?', self.is_character_nearby, 3)
+        a1 = Action('캐릭터 공격', self.attack_character)
+        a2 = Action('캐릭터 추적', self.move_to_character)
 
-        a2 = Action('랜덤 위치 설정', self.set_random_location)
-        a3 = Action('목표 위치로 이동', self.move_to_target)
-        wander = Sequence('배회', a2, a3)
+        attack_or_chase = Sequence('공격 또는 추적', c1,
+                                   Selector('공격 우선',
+                                            Sequence('공격', c2, a1),
+                                            a2))
 
-        root = Selector('추적 또는 배회', chase_character, wander)
+        a3 = Action('랜덤 위치 설정', self.set_random_location)
+        a4 = Action('목표 위치로 이동', self.move_to_target)
+        wander = Sequence('배회', a3, a4)
+
+        root = Selector('추적/공격 또는 배회', attack_or_chase, wander)
         self.bt = BehaviorTree(root)
 
     def distance_less_than(self, x1, y1, x2, y2, r):
