57a4142c3d05e16da78dc87332237510744b0d92
jundole05 <jundole05@gmail.com>
2025-12-06 21:42:05 +0900

    포탈 구현 완료

---
+++ portal.py
@@ -0,0 +1,33 @@
+from pico2d import *
+
+class Portal:
+    def __init__(self, x, y, width=100, height=100):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.active = False  # 레벨 2가 되면 활성화
+
+    def update(self):
+        pass
+
+    def draw(self):
+        # 투명 포탈이지만 디버깅용 바운딩박스 (초록색)
+        if self.active:
+            left = self.x - self.width // 2
+            right = self.x + self.width // 2
+            bottom = self.y - self.height // 2
+            top = self.y + self.height // 2
+            draw_rectangle(left, bottom, right, top, 0, 255, 0)
+
+    def get_bb(self):
+        return (self.x - self.width // 2,
+                self.y - self.height // 2,
+                self.x + self.width // 2,
+                self.y + self.height // 2)
+
+    def activate(self):
+        self.active = True
+
+    def handle_collision(self, group, other):
+        pass
